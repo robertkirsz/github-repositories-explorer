426516999dc4b06f2eac9a0e7e786e914c40ec54
Robert Kirsz <robert.kirsz@gmail.com>
2020-09-27 19:43:01 +0200

    Move search logic out of App

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
 import Div from 'styled-kit/Div'
-import store from 'store'
-import { fetchUsers, clearUsers } from 'reducers/users'
-import DebouncedInput from 'components/DebouncedInput'
 import UsersList from 'components/UsersList'
 import SearchInfo from 'components/SearchInfo'
 import SearchMoreButton from 'components/SearchMoreButton'
-import { useSelector } from 'react-redux'
+import SearchUsersInput from 'components/SearchUsersInput'
 
 export default function App() {
-  const { lastSearchedUsername } = useSelector(({ users }) => users)
-
-  function handleSearch(username) {
-    if (username) store.dispatch(fetchUsers({ username }))
-    else store.dispatch(clearUsers())
-  }
-
   return (
     <Div column listTop width="100%" maxWidth={480} padding={8} css="overflow: hidden;">
       <h1 css="text-align: center;">GitHub Repositories Explorer</h1>
-      <DebouncedInput placeholder="Enter username" value={lastSearchedUsername} onChange={handleSearch} />
+      <SearchUsersInput />
       <SearchInfo />
       <UsersList />
       <SearchMoreButton />
---
+++ src/components/SearchUsersInput.js
@@ -0,0 +1,15 @@
+import store from 'store'
+import { fetchUsers, clearUsers } from 'reducers/users'
+import DebouncedInput from 'components/DebouncedInput'
+import { useSelector } from 'react-redux'
+
+export default function SearchUsersInput() {
+  const { lastSearchedUsername } = useSelector(({ users }) => users)
+
+  function handleSearch(username) {
+    if (username) store.dispatch(fetchUsers({ username }))
+    else store.dispatch(clearUsers())
+  }
+
+  return <DebouncedInput placeholder="Enter username" value={lastSearchedUsername} onChange={handleSearch} />
+}
